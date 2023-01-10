9a195784fa0a1e3a68588fe8e7772e72e973cab1
Jaime <alu0101329093@ull.edu.es>
2023-01-10 19:10:52 +0000

    feat: Add combination function

---
+++ src/combination_function.cpp
@@ -0,0 +1,21 @@
+#include "combination_function.h"
+
+namespace cc {
+
+CombinationFunction::CombinationFunction(PRFPtr first_function,
+                                         PRFPtr second_function)
+    : first_function_{std::move(first_function)},
+      second_function_{std::move(second_function)} {}
+
+std::vector<int> CombinationFunction::Evaluate(
+    const std::vector<int>& args) const {
+  std::vector<int> results{first_function_->Evaluate(args)};
+  std::vector<int> second_function_results{second_function_->Evaluate(args)};
+
+  results.insert(results.end(), second_function_results.begin(),
+                 second_function_results.end());
+
+  return results;
+}
+
+}  // namespace cc
