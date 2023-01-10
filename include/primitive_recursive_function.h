e409d62f378a8c670d93401881c0cd32b7481a4b
jaime <alu0101329093@ull.edu.es>
2023-01-10 09:44:31 +0000

    feat: Add primitive recursive function abstraction

---
+++ include/primitive_recursive_function.h
@@ -0,0 +1,18 @@
+#ifndef P3PRIMITIVERECURSIVEFUNCTIONS_PRIMITIVE_RECURSIVE_FUNCTION_H_
+#define P3PRIMITIVERECURSIVEFUNCTIONS_PRIMITIVE_RECURSIVE_FUNCTION_H_
+
+#include <memory>
+#include <vector>
+
+namespace cc {
+
+class PrimitiveRecursiveFunction {
+ public:
+    virtual std::vector<int> evaluate(const std::vector<int>& args) = 0;
+};
+
+using PRFPtr = std::shared_ptr<PrimitiveRecursiveFunction>;
+
+}  // namespace cc
+
+#endif  // P3PRIMITIVERECURSIVEFUNCTIONS_PRIMITIVE_RECURSIVE_FUNCTION_H_
